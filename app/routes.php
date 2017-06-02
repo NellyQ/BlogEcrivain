da56ed216b9115975d53a2bf55058c292272dade
Nelly <nelly.quesada@orange.fr>
2017-06-02 10:01:14 +0200

    Intégration de Twig (modification de composer, création de index.html.twig pour remplacer views.php, modification de routes.php pour afficher la nouvelle vue)

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@
 // Home page
 $app->get('/', function () use ($app) {
     $billets = $app['dao.billet']->findAll();
+     return $app['twig']->render('index.html.twig', array('billets' => $billets));
 
     ob_start();             // start buffering HTML output
     require '../views/view.php';
